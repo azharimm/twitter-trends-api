eba6d4c1f5d4771beb320f50d8da2a171d99183f
Azhari Marzan <azhari.marzan@gmail.com>
2021-03-15 12:44:01 +0700

    ^ trends based location

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -12,44 +12,58 @@ exports.index = (req, res) => {
 };
 
 exports.trends = async (req, res) => {
-    const htmlResult = await request.get(`${process.env.BASE_URL}`);
-    const $ = await cheerio.load(htmlResult);
-    const trends = [];
-    const location = 'Worldwide';
-    $(".trend-card").each((index, el) => {
-        let time = $(el).find(".trend-card__time").text();
-        let temp = [];
-        $(el)
-            .find(".trend-card__list")
-            .find("li")
-            .each((key, el) => {
-                let name = $(el).find("a").text();
-                let tweet_count = $(el).find(".tweet-count").text();
-                temp.push({ name, tweet_count });
-                trends[index] = {
-                    time: time,
-                    data: temp,
-                };
-            });
-    });
+    const { location } = req.query;
+    let baseUrl = `${process.env.BASE_URL}`;
+    let locationName = "Worldwide";
+    if (location) {
+        baseUrl = baseUrl + "/"+ location;
+        locationName = location.toUpperCase();
+    }
+    try {
+        const htmlResult = await request.get(baseUrl);
+        const $ = await cheerio.load(htmlResult);
+        const trends = [];
+        $(".trend-card").each((index, el) => {
+            let time = $(el).find(".trend-card__time").text();
+            let temp = [];
+            $(el)
+                .find(".trend-card__list")
+                .find("li")
+                .each((key, el) => {
+                    let name = $(el).find("a").text();
+                    let tweet_count = $(el).find(".tweet-count").text();
+                    temp.push({ name, tweet_count });
+                    trends[index] = {
+                        time: time,
+                        data: temp,
+                    };
+                });
+        });
 
-    return json(res, {location, trends});
+        return json(res, { location: locationName, trends });
+    } catch (error) {
+        return errorJson(res, error);
+    }
 };
 
 exports.location = async (req, res) => {
-    const fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
-    const htmlResult = await request.get(`${process.env.BASE_URL}`);
-    const $ = await cheerio.load(htmlResult);
-    const locations = [];
-    $(".suggested-locations__list")
-        .children("li")
-        .each((index, el) => {
-            let location = $(el).text();
-            locations.push({
-                location_path: location.toLocaleLowerCase(),
-                location_url: '/trends?location='+location.toLocaleLowerCase(),
+    try {
+        const htmlResult = await request.get(`${process.env.BASE_URL}`);
+        const $ = await cheerio.load(htmlResult);
+        const locations = [];
+        $(".suggested-locations__list")
+            .children("li")
+            .each((index, el) => {
+                let location = $(el).text();
+                locations.push({
+                    location_path: location.toLocaleLowerCase(),
+                    location_url:
+                        "/trends?location=" + location.toLocaleLowerCase(),
+                });
             });
-        });
 
-    return json(res, locations);
+        return json(res, locations);
+    } catch (error) {
+        return errorJson(res, error);
+    }
 };
